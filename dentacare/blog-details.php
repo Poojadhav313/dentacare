f5ea8d66b498b45cc0e89a3aa27728b4637dab05
Pooja <poojadhav313@gmail.com>
2024-12-11 00:06:51 +0530

    blog details - main section complete

---
+++ dentacare/blog-details.php
@@ -0,0 +1,89 @@
+<?php include('header.html'); ?>
+
+<head>  
+  <title>Dentacare - Oral health and the</title>
+  <link rel="stylesheet" href="blog-details.css">
+</head>
+
+
+<body>
+
+  <section id="banner-section">
+    <p class="banner-p">TREATMENT &#8226; October 4, 2024</p>
+    <h1>Oral health and the challenges of living with teeth</h1>
+  </section>
+
+
+  <section id="main-section">
+    <h2>We offer a full range of dental services designed to improve and maintain oral health. Whether you’re visiting for a regular cleaning,need restorative work, or are looking to enhance your smile</h2>  
+
+    <p class="blue-left-line">
+    Integer suscipit vulputate non lacus eget mollis scelerisque ullamcorper eu. Lorem nec leo eget sit nulla massa sed mauris. In elit non id volutpat sagittis sapien faucibus duis. Diam sociis ac sed sollicitudin amet placerat. Id aliquet risus nam et nunc mollis libero laoreet sed. Penatibus risus viverra Amet vivamus.
+    </p>
+
+    <img src="https://cdn.prod.website-files.com/66ff6ff93d2a162b38251e51/671f2f98c499f9363e7f634c_66ffdca8b94ceb7efb832833_66ffdbd41c25ea447d248847_Blog%252520Details%252520Mouth%252520Image.jpeg" alt="">
+
+    <h2 class="no-margin">We understand that visiting the dentist can be daunting, and that’s why we place such a high priority</h2>
+
+    <p>Viverra consectetur cras risus ornare molestie volutpat ornare. Fermentum ultrices quam suspendisse loremTortor nibh mattis tortor elit dignissim sapien ante pellentesque fames Lacus est lorem eu donec Quis quisque vitae amet imperdiet placerat cras. Amet et fermentum id euismod elementum id Egestas aliquet malesuada.</p>
+
+    <img class="img2" src="https://img.freepik.com/premium-photo/blue-abstract-banner-background_181182-20850.jpg" alt="">
+
+    <h2 class="no-margin">We believe quality dental care should be accessible to everyone</h2>
+
+    <p class="blue-left-line no-margin">Lorem ipsum dolor sit amet consectetur. Varius tincidunt in euismod velit adipiscing auctor ullamcorper. Facilisi malesuada commodo urna rhoncus nunc commodo hendrerit amet lectus. Nulla commodo sed leo eu lectus diam pulvinar nascetur. Faucibus lacinia mi eu viverra pharetra odio. Sed suspendisse gravida.</p>
+
+    <ul>
+      <li>Tortor mattis tempus dui praesent non dui sed.</li>
+      <li>Morbi phasellus dictum feugiat malesuada condimentum.</li>
+      <li>In aenean quis metus egestas Tristique sed vulput.</li>
+    </ul>
+
+    <hr class="small-hr">
+
+    <div id="expert-section">
+      <img src="https://cdn.prod.website-files.com/66ff6ff93d2a162b38251e51/6721b8a6180d545903885f0c_Blog%20Icon.svg" alt="">
+      <div id="expert-data">
+        <h2>Davis Septimus <span class="expert-tag">Surgery Expert</span></h2>
+        <p>"Seader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."</p>
+      </div>
+    </div>
+
+    <hr class="long-hr">
+
+    <div id="news-section">
+      <div id="news-title">
+        <h1>Recent news</h1>
+        <a href="">Browse all posts</a>
+      </div>
+
+      <div id="news-boxes">
+        <div class="box">
+          <img src="https://cdn.prod.website-files.com/66ff6ff93d2a162b38251e51/671f311872eb865b8589dc5a_Blog%20Six%20Pg-p-500.jpg" alt="">
+          <div class="date">Dentistry &#8226; October 30, 2024</div>
+          <h3>How to Recognize Early Signs of Gum Disease</h3>
+          <p>Lorem Ipsum donec phasellus vitae turpis condimentum cursus tempus. Nec curabitur in dictum et. Tellus eu nullam</p>
+          <a href="">Read More</a>
+        </div>
+
+
+        <div class="box">
+          <img src="https://cdn.prod.website-files.com/66ff6ff93d2a162b38251e51/671f30c898ab71bab31b4a36_Blog%20Five%20Pg-p-500.jpg" alt="">
+          <div class="date">Dentistry &#8226; October 30, 2024</div>
+          <h3>Understanding the importance of gum health</h3>
+          <p>Lorem Ipsum donec phasellus vitae turpis condimentum cursus tempus. Nec curabitur in dictum et. Tellus eu nullam</p>
+          <a href="">Read More</a>
+        </div>
+
+      </div>
+
+
+
+    </div>
+
+  </section>
+
+
+
+
+<?php include('footer.html'); ?>
