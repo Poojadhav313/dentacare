0cc6c62cef48035b77ead36791794e5444cb685b
Pooja <poojadhav313@gmail.com>
2024-12-11 15:13:09 +0530

    service - banner complete

---
+++ dentacare/service.php
@@ -0,0 +1,23 @@
+<?php include('header.html') ?>
+
+<head>
+  <title>Dentacare - Service</title>
+  <link rel="stylesheet" href="service.css">
+</head>
+
+<body>
+
+<section id="banner-section">
+    <p class="banner-p">OUR Services</p>
+    <h1>Our Services</h1>
+    <p>Lorem ipsum dolor sit amet consectetur. Condimentum pellentesque enim felis blandit ornare cursus quisque. Sagittis at a eget suscipit mattis nulla egestas porttitor massa.</p>
+</section>
+
+
+
+
+
+
+<div class="space" style="height: 500px;"></div>
+
+<?php include('footer.html') ?>
