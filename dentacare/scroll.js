9f2b876290aeb87d69a98455ebefcca8dd2b1f33
Pooja <poojadhav313@gmail.com>
2024-12-14 15:20:16 +0530

    navbar - linking, scroll-color complete

--- dentacare/scroll.js
+++ dentacare/scroll.js
@@ -39,3 +39,25 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener('scroll', scrollAnimation);
   scrollAnimation(); 
 });
+
+
+
+////////////////////////////// navbar color change when scroll //////////////////////////////
+
+var nav = document.getElementById('nav');
+
+var navHeight = nav.offsetTop;
+
+
+window.addEventListener('scroll', () =>{
+  if(window.scrollY > navHeight){
+    nav.style.background = 'black';
+  }
+  
+  if(window.scrollY == 0){
+    nav.style.background = 'linear-gradient(rgba(206, 205, 205, 0.5), rgba(204, 204, 204, 0.5)),url(https://img.freepik.com/premium-photo/beautiful-girl-treated-teeth_88340-5103.jpg?w=740)';
+    nav.style.backgroundRepeat = 'no-repeat';
+    nav.style.backgroundPosition = 'top';
+    nav.style.backgroundSize = 'cover';
+  }
+})
--- dentacare/testimonial.js
+++ dentacare/testimonial.js
@@ -56,4 +56,4 @@ function prevBtnClick()
 
 
 
-console.log(slides);
+//console.log(slides);
