003a61827c8a63fb76987dc62560ac51bf1ace94
Pooja <poojadhav313@gmail.com>
2024-12-14 11:29:21 +0530

    about - banner section complete

---
+++ dentacare/about.php
@@ -0,0 +1,23 @@
+<?php include('header.html') ?>
+
+<head>  
+  <title>Dentacare - About</title>
+  <link rel="stylesheet" href="about.css">
+</head>
+
+
+<body>
+
+  <section id="banner-section">
+    <p class="banner-p">About us</p>
+    <h1>About Dentacare</h1>
+    <p>Lorem ipsum dolor sit amet consectetur. Condimentum pellentesque enim felis blandit ornare cursus quisque. Sagittis at a eget suscipit mattis nulla egestas porttitor massa.</p>  
+  </section>
+
+
+
+
+
+  <div class="space" style="height: 500px;"></div>
+
+<?php include('footer.html'); ?>
