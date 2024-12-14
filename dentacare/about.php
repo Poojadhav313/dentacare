b117bc922955489fc284b0a9d07d224749f88adc
Pooja <poojadhav313@gmail.com>
2024-12-14 12:26:44 +0530

    about - hover section complete

--- dentacare/about.php
+++ dentacare/about.php
@@ -23,7 +23,83 @@
     </div>
   </section>
 
+  <section id="hover-section">
+    <section id="section-title">
+      <p>Benefits of our salon</p>
+    </section>
 
+    <a href="service.php">
+    <div id="section1">
+        <div class="hover-section">
+          <h1>Smile makeovers</h1>
+        </div>
+        <div class="img">
+          <img src="" alt="">          
+          <!-- <img src="https://cdn.prod.website-files.com/66ff6ff93d2a162b38251e51/671f311872eb865b8589dc5a_Blog%20Six%20Pg.jpg" alt=""> -->
+        </div>
+        <div class="arrow">
+        &#8594;
+        </div>
+    </div>
+    </a>
+
+    <a href="service.php">
+    <div id="section2">
+        <div class="hover-section">
+          <h1>Wisdom Teeth Removal</h1>
+        </div>
+        <div class="img">
+          <img src="" alt="">
+      </div>
+      <div class="arrow">
+      &#8594;
+        </div>
+    </div>
+    </a>
+
+    <a href="service.php">
+    <div id="section3">
+        <div class="hover-section">
+          <h1>Crowns and Bridges</h1>
+        </div>
+        <div class="img">
+          <img src="" alt="">
+      </div>
+      <div class="arrow">
+      &#8594;
+        </div>
+    </div>
+    </a>
+
+    <a href="service.php">
+    <div id="section4">
+        <div class="hover-section">
+          <h1>Digital X-Rays</h1>
+        </div>
+        <div class="img">
+          <img src="" alt="">
+      </div>
+      <div class="arrow">
+      &#8594;
+      </div>
+    </div> 
+    </a>
+
+    <a href="service.php">
+    <div id="section5">
+        <div class="hover-section">
+          <h1>Gum Health Evaluation</h1>
+        </div>
+        <div class="img">
+          <img src="" alt="">
+      </div>
+      <div class="arrow">
+      &#8594;
+      </div>
+    </div>   
+    </a>
+
+  </section>
 
   <div class="space" style="height: 500px;"></div>
 
